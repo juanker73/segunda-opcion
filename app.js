095365d8d3593a4612cbcb975341f88b39620435
juanker73 <114301531+juanker73@users.noreply.github.com>
2023-04-15 17:42:53 -0300

    primer commit creacion de archivos

---
+++ app.js
@@ -0,0 +1,26 @@
+
+const API_BASE = "https://rickandmortyapi.com/api"
+const API_CHARACTERS = "https://rickandmortyapi.com/api/character"
+
+const llamadaApi = fetch(API_CHARACTERS);
+
+llamadaApi.then((data) => {
+    return data.json()
+}).then((data) => {
+    const $characters = document.getElementById("container-grid")
+    console.log(data.results[0])
+    for(let i = 0; i < data.results.length; i++) {
+        
+        $characters.innerHTML += `
+        <div class="grid-item">
+                <img class"character-image" src=${data.results[i].image} alt="imagen de personaje"/>
+                <h2>${data.results[i].name}</h2>
+                <p>Gender: ${data.results[i].gender}</p>
+                <p>Species: ${data.results[i].species}</p>
+                <p>Status: ${data.results[i].status}</p>
+
+        `
+    }
+}).catch((error) => {
+    console.log(error)
+})
